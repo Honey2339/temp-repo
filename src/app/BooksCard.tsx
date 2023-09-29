3b5f5be1bbac2be7e224cdcae38f259808e4d96e
Honey2339 <prasoon2study@gmail.com>
2023-09-29 19:04:08 +0530

    Update BooksCard.tsx

--- src/app/BooksCard.tsx
+++ src/app/BooksCard.tsx
@@ -16,21 +16,37 @@ import Image from "next/image";
 
 // https://www.googleapis.com/books/v1/volumes
 
+type VolumeInfo = {
+  title: string;
+  publisher: string;
+  description: string;
+  imageLinks?: {
+    smallThumbnail: string;
+  };
+  infoLink: string;
+};
+
+type ApiDataItem = {
+  volumeInfo: VolumeInfo;
+};
+
+type ApiData = ApiDataItem[];
+
 function BooksCard() {
-  const apiData = useRecoilValue(apiDataState);
+  const apiData: ApiData = useRecoilValue(apiDataState);
 
   return (
     <div className="grid grid-cols-3 gap-9 mt-10">
       {apiData &&
         apiData.map((data, idx) => (
-          <Card className="flex max-w-md">
+          <Card key={idx} className="flex max-w-md">
             <Image
               height={50}
               width={150}
               alt="https://picsum.photos/200"
               src={
-                data.volumeInfo.imageLinks
-                  ? data.volumeInfo.imageLinks.smallThumbnail
+                data.volumeInfo?.imageLinks
+                  ? data.volumeInfo?.imageLinks?.smallThumbnail
                   : "https://picsum.photos/200"
               }
               className="rounded-xl"
@@ -38,21 +54,23 @@ function BooksCard() {
             <div className="flex flex-col justify-between ml-2">
               <div>
                 <CardHeader>
-                  <CardTitle>{data.volumeInfo.title}</CardTitle>
-                  <CardDescription>{data.volumeInfo.publisher}</CardDescription>
+                  <CardTitle>{data.volumeInfo?.title}</CardTitle>
+                  <CardDescription>
+                    {data.volumeInfo?.publisher}
+                  </CardDescription>
                 </CardHeader>
                 <CardContent>
                   <p>
-                    {data.volumeInfo.description
+                    {data.volumeInfo?.description
                       ? `${
-                          data.volumeInfo.description.substring(0, 100) + "..."
+                          data.volumeInfo?.description.substring(0, 100) + "..."
                         }`
-                      : data.volumeInfo.description}
+                      : data.volumeInfo?.description}
                   </p>
                 </CardContent>
               </div>
               <div className="mt-2">
-                <a href={data.volumeInfo.infoLink}>
+                <a href={data.volumeInfo?.infoLink}>
                   <Button>More {"->"}</Button>
                 </a>
               </div>
